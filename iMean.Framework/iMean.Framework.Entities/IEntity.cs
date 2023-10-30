20058faf75cb2311a64ffe6c70bf3cf66c4aa584
iMean <amine.bekkali@protonmail.com>
2023-10-30 16:02:07 +0100

    Adding Stylecop.Analyzers + Generating nuspec for Entities csproj

---
+++ iMean.Framework/iMean.Framework.Entities/IEntity.cs
@@ -0,0 +1,15 @@
+﻿#pragma warning disable SA1300 // Element should begin with upper-case letter
+namespace iMean.Framework.Entities
+#pragma warning restore SA1300 // Element should begin with upper-case letter
+{
+    /// <summary>
+    /// Represents an Entity contract defined by its unique identifier.
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// Gets an entity by its unique identifier.
+        /// </summary>
+        int Id { get; init; }
+    }
+}
